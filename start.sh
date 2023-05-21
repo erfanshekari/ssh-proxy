a4b91fe98bb66e7d9f42f9e8715ca816a0f73cbb
erfan <erfan.dp.co@gmail.com>
2023-05-21 07:58:23 +0330

    LAN subnet added

--- start.sh
+++ start.sh
@@ -9,13 +9,13 @@ connect_to_sshuttle() {
     if [ -z "$SSH_KEY" ]; then
         sshuttle --dns -r $SSH_USER@$SSH_HOST 0.0.0.0/0 --method tproxy \
             --ssh-cmd "sshpass -p $SSH_PASSWORD ssh -o "StrictHostKeyChecking=no" -p $SSH_PORT" \
-            -x $SSH_HOST -x 127.0.0.1 -x 172.17.0.0/16 -x 172.21.0.0/24 -x 172.22.0.0/24 -x 172.23.0.0/24 -x 10.17.0.0/24 \
+            -x $SSH_HOST -x 127.0.0.1 -x 172.17.0.0/16 -x 172.21.0.0/24 -x 172.22.0.0/24 -x 172.23.0.0/24 -x 10.17.0.0/24 -x 192.168.1.0/24 \
             --disable-ipv6
     else
-        chmod 600 $SSH_KEY
+        chmod 600 /app/key.pem
         sshuttle --dns -r $SSH_USER@$SSH_HOST 0.0.0.0/0 --method tproxy \
-            --ssh-cmd "ssh -o "StrictHostKeyChecking=no" -i $SSH_KEY -p $SSH_PORT" \
-            -x $SSH_HOST -x 127.0.0.1 -x 172.17.0.0/16 -x 172.21.0.0/24 -x 172.22.0.0/24 -x 172.23.0.0/24 -x 10.17.0.0/24 \
+            --ssh-cmd "ssh -o "StrictHostKeyChecking=no" -i /app/key.pem -p $SSH_PORT" \
+            -x $SSH_HOST -x 127.0.0.1 -x 172.17.0.0/16 -x 172.21.0.0/24 -x 172.22.0.0/24 -x 172.23.0.0/24 -x 10.17.0.0/24 -x 192.168.1.0/24 \
             --disable-ipv6
     fi
 
