c30032f1d000d418d029c9bc23d81311f2880f13
Erfan <erfan.dp.co@gmail.com>
2023-08-15 14:38:02 +0330

    ssh-proxy command fix

--- socks/start.ssh.sh
+++ socks/start.ssh.sh
@@ -2,8 +2,8 @@
 
 
 if [ -z "$SSH_KEY" ]; then
-    sshpass -p $SSH_PASSWORD ssh -p $SSH_PORT -o "StrictHostKeyChecking=no" -tt $SSH_USER@$SSH_HOST -D 0.0.0.0:1089
+    sshpass -p $SSH_PASSWORD ssh -p $SSH_PORT -o "StrictHostKeyChecking=no" -tt $SSH_USER@$SSH_HOST -v -v -D 0.0.0.0:1089
 else
     chmod 600 /app/key.pem
-    ssh -p $SSH_PORT -o "StrictHostKeyChecking=no" -tt -i /app/key.pem $SSH_USER@$SSH_HOST -D 0.0.0.0:1089
+    ssh -p $SSH_PORT -o "StrictHostKeyChecking=no" -tt -i /app/key.pem $SSH_USER@$SSH_HOST -v -v -D 0.0.0.0:1089
 fi
